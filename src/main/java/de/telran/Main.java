253bc5d08044312accfeff59b1118fd62574ab14
yevhenii.ashurov <yevhenii.ashurov@occe>
2023-09-06 22:14:32 +0200

    add lesson8 - Home_Work

--- src/main/java/de/telran/Main.java
+++ src/main/java/de/telran/Main.java
@@ -1,11 +1,4 @@
 package de.telran;
 
 public class Main {
-    public static void main(String[] args) {
-        byte x = 100; // 0110.0100
-        int y = 100; // 0000.0000 - 0000.0000 - 0000.0000 - 0110.0100
-        int z = 10000; // 0000.0000 0000.0000 0010.0111 0001.0000
-        byte n = (byte) z;
-        System.out.println(n);
-    }
 }
---
+++ src/main/java/de/telran/lesson8/IfElseSorting.java
@@ -0,0 +1,33 @@
+package de.telran.lesson8;
+
+import java.util.Random;
+
+public class IfElseSorting {
+    public static void main(String[] args) {
+
+        Random rnd = new Random();
+
+        int x = rnd.nextInt(1000);
+        int y = rnd.nextInt(1000);
+        int z = rnd.nextInt(1000);
+
+        if (x <= y && y <= z) {
+            printSort(x, y, z);
+        } else if (x <= z && z <= y) {
+            printSort(x, z, y);
+        } else if (y <= x && x <= z) {
+            printSort(y, x, z);
+        } else if (y <= z) {
+            printSort(y, z, x);
+        } else if (x <= y) {
+            printSort(z, x, y);
+        } else {
+            printSort(z, y, x);
+        }
+    }
+
+    static void printSort(int min, int mid, int max) {
+        System.out.println("По возрастанию: " + min + " " + mid + " " + max);
+        System.out.println("По убыванию: " + max + " " + mid + " " + min);
+    }
+}
